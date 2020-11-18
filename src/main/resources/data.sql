20b0d8e9987e9d2ea02f8621be6db1a80cfed4cd
jairoandres <andres.granada98@gmail.com>
2020-11-17 21:52:53 -0500

    Modified data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,9 +2,9 @@ INSERT INTO estudiante (cedula,nombre,promedio)
 VALUES  (000,'Yusep',5.0),
         (111,'Andres', 3.1),
         (222,'Luis',4.0),
-        (333,'Pepito',1.1),
-        (444,'Yusep',5.0),
-        (555,'Andres', 3.0),
-        (666,'Luis',4.2),
-        (777,'Pepito',2.5),
-        (888,'Javier',2.0);
+        (333,'Sebastian',1.1),
+        (444,'Angie',5.0),
+        (555,'Bob', 3.0),
+        (666,'Megan',4.2),
+        (777,'Sergio',2.5),
+        (888,'Mateo',2.0);
